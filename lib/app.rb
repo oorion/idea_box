76f6206469c61aac96942c538216036d8330eeb9
Orion Osborn <orion.osborn@gmail.com>
2014-12-16 14:46:16 -0700

    initial commit

---
+++ lib/app.rb
@@ -0,0 +1,38 @@
+require './idea'
+require './idea_store'
+
+class IdeaBoxApp < Sinatra::Base
+  set :method_override, true
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb :index, locals: {ideas: IdeaStore.all, idea: Idea.new(params)} #not sure why the idea is needed here
+  end
+
+  post '/' do
+    idea = IdeaStore.create(params[:idea])
+    redirect '/'
+  end
+
+  get '/:id/edit' do |id|
+    idea = IdeaStore.find(id.to_i)
+    erb :edit, locals: {id: id, idea: idea}
+  end
+
+  put '/:id' do |id|
+    IdeaStore.update(id.to_i, params[:idea])
+    redirect '/'
+  end
+
+  delete '/:id' do |id|
+    IdeaStore.delete(id.to_i)
+    redirect '/'
+  end
+
+  not_found do
+    erb :error
+  end
+end
